e3e95c5c7b8f01596ee5e8b0f655d26530aa05a1
tantatinta <bluekasia@gmail.com>
2020-05-15 15:52:31 -0400

    added skeleton js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,28 @@ const uiController = (() => {
 })()
 
 //global app controller
-const controller = (()=> {
-  
-})()
+const controller = ((budgetCtrl, uiCtrl)=> {
+
+  const controlAddItem = () => {
+    console.log("inside ctrl add")
+    //get input data
+    
+    //add item to budget controller
+
+    //add item to ui
+
+    //calculate budget
+
+    //display budget
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', controlAddItem);
+
+  document.addEventListener('keypress', (event) => {
+    if(event.keyCode === 13 || event.which === 13) {
+      controlAddItem();
+    }    
+  })
+
+
+})(budgetController, uiController)
